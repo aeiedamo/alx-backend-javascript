b665443c311f3e9743ab89c94febe1a35f973881
AlaEldin Mohamed <aeiedamo@gmail.com>
2024-09-21 14:48:03 +0100

    0x05 - fixes to styles

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,13 @@
 const fn1 = process.stdout;
 const fn2 = process.stdin;
 
-fn1.write("Welcome to Holberton School, what is your name?\n");
-fn2.on("data", (data) => {
+fn1.write('Welcome to Holberton School, what is your name?\n');
+fn2.on('data', (data) => {
   fn1.write(`Your name is: ${data}`);
 });
 
-fn2.on("end", () => {
-  fn1.write("This important software is now closing\n");
+fn2.on('end', () => {
+  fn1.write('This important software is now closing\n');
 });
 
 fn2.resume();
